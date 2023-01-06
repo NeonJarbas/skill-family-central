bc371829557fcb27beb7c056b66c15fae291a976
jarbasai <jarbasai@mailfence.com>
2023-01-06 12:31:09 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ class FamilyCentralSkill(OVOSCommonPlaybackSkill):
 
     def initialize(self):
         url = "https://www.youtube.com/channel/UCvRI1Nyvj9pAyHPzX7AviWQ"
-        bootstrap = f"https://raw.githubusercontent.com/OpenJarbas/streamindex/main/{self.archive.db.name}.json"
+        bootstrap = "https://github.com/JarbasSkills/skill-family-central/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
         self.archive.monitor(url)
         self.archive.setDaemon(True)
